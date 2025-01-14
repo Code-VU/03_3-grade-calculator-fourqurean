b5dea2f420667cafdbca16948ae804ac21ca93b7
Christopher Fourqurean <Chris.Fourqurean@vu.com>
2025-01-14 17:13:48 -0600

    should be good

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -26,13 +26,6 @@ def calculateGrade():
 
     # end assignment
 
-# Score   Grade
-# >= 0.9     A
-# >= 0.8     B
-# >= 0.7     C
-# >= 0.6     D
-# < 0.6     F
-
 ## If you want to test locally before you try to sync
 ## Run > python calculateGrade.py
 
