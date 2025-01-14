915bfdb000066804236fe4043c841ac1456422bb
Christopher Fourqurean <Chris.Fourqurean@vu.com>
2025-01-14 17:12:12 -0600

    first try

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -3,9 +3,35 @@ def calculateGrade():
     print("Calculating Grade")
     # This first line is provided for you
 
-    hrs = float(input("Enter score:"))
+    # User input for score
+    try:
+        score_input = float(input("Enter a score between 9.9 and 0.0: "))
+    except:
+        print("Bad score")
+        return
+    
+    # Scoring area
+    if score_input >= 10.0 :
+        print("Bad score")
+    elif score_input >= 0.9 :
+        print("A")
+    elif score_input >= 0.8 :
+        print("B")
+    elif score_input >= 0.7 :
+        print("C")
+    elif score_input >= 0.6 :
+        print("D")
+    else :
+        print("F")
 
     # end assignment
+
+# Score   Grade
+# >= 0.9     A
+# >= 0.8     B
+# >= 0.7     C
+# >= 0.6     D
+# < 0.6     F
 
 ## If you want to test locally before you try to sync
 ## Run > python calculateGrade.py
